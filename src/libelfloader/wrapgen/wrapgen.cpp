b3a0ce1e88b993cfe37cadaf1d5016a1d3a4cfad
TheBrokenRail <connor24nolan@live.com>
2020-01-18 19:24:26 -0500

    Remove the need for LD_LIBRARY_PATH when using PulseAudio

--- src/libelfloader/wrapgen/wrapgen.cpp
+++ src/libelfloader/wrapgen/wrapgen.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <set>
+#include <vector>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -22,18 +23,53 @@
 // TODO: use wrapgen32 to generate 32-bit wrappers.
 
 void parse_elf(const char* elf, std::string& soname_out, std::set<std::string>& symbols_out);
-void generate_wrapper(std::ofstream& output, const char* soname, const std::set<std::string>& symbols);
+void generate_wrapper(std::ofstream& output, const char* soname, const std::set<std::string>& symbols, const std::vector<std::string>& dependencies);
+
+std::string get_library_path(std::string elfLibrary)
+{
+    if (access(elfLibrary.c_str(), R_OK) == -1)
+	{
+		// Try loading the library and then ask the loader where it found the library.
+		// It is simpler than parsing /etc/ld.so.conf.
+
+		void* handle = dlopen(elfLibrary.c_str(), RTLD_LAZY | RTLD_LOCAL);
+		char path[PATH_MAX];
+
+		if (!handle)
+		{
+			std::stringstream ss;
+			ss << "Cannot load " << elfLibrary << ": " << dlerror();
+			throw std::runtime_error(ss.str());
+		}
+
+		if (dlinfo(handle, RTLD_DI_ORIGIN, path) == 0)
+		{
+			elfLibrary.insert(0, "/");
+			elfLibrary.insert(0, path);
+		}
+		else
+		{
+			std::stringstream ss;
+			std::cerr << "Cannot locate " << elfLibrary << ": " << dlerror();
+			throw std::runtime_error(ss.str());
+		}
+
+		dlclose(handle);
+	}
+	return elfLibrary;
+}
 
 int main(int argc, const char** argv)
 {
 	std::string elfLibrary;
 	std::set<std::string> symbols;
+	std::vector<std::string> dependencies;
 	std::string soname;
 	std::ofstream output;
 
-	if (argc != 3)
+	if (argc < 3)
 	{
-		std::cerr << "Usage: " << argv[0] << " <library-name> <output-file>\n";
+		std::cerr << "Usage: " << argv[0] << " <library-name> <output-file> [library-non-standard-dependencies]\n";
 		return 1;
 	}
 
@@ -45,38 +81,17 @@ int main(int argc, const char** argv)
 		if (!output.is_open())
 			throw std::runtime_error("Cannot open output file");
 
-		if (access(elfLibrary.c_str(), R_OK) == -1)
+		elfLibrary = get_library_path(elfLibrary);
+		for (int i = 3; i < argc; i++)
 		{
-			// Try loading the library and then ask the loader where it found the library.
-			// It is simpler than parsing /etc/ld.so.conf.
-
-			void* handle = dlopen(elfLibrary.c_str(), RTLD_LAZY | RTLD_LOCAL);
-			char path[PATH_MAX];
-
-			if (!handle)
-			{
-				std::stringstream ss;
-				ss << "Cannot load " << elfLibrary << ": " << dlerror();
-				throw std::runtime_error(ss.str());
-			}
-
-			if (dlinfo(handle, RTLD_DI_ORIGIN, path) == 0)
-			{
-				elfLibrary.insert(0, "/");
-				elfLibrary.insert(0, path);
-			}
-			else
-			{
-				std::stringstream ss;
-				std::cerr << "Cannot locate " << elfLibrary << ": " << dlerror();
-				throw std::runtime_error(ss.str());
-			}
-
-			dlclose(handle);
+		    // The Depedencies argument is for ELF depedencies thatare located in a non-standard location, other depedencies are found automatically.
+		    // Because they are stored in a non-standard location they must be an absolute path, and therefore must have /Volumes/SystemRoot appeneed to the front so Darling can find it.
+		    // This is need because RPath doesn't work because all of the libraries are in /Volumes/SystemRoot instead of /.
+		    dependencies.push_back(std::string(argv[i]).insert(0, "/Volumes/SystemRoot"));
 		}
 
 		parse_elf(elfLibrary.c_str(), soname, symbols);
-		generate_wrapper(output, soname.c_str(), symbols);
+		generate_wrapper(output, soname.c_str(), symbols, dependencies);
 	}
 	catch (const std::exception& e)
 	{
@@ -236,20 +251,49 @@ end_dyn:
 	munmap((void*) ehdr, length);
 }
 
-void generate_wrapper(std::ofstream& output, const char* soname, const std::set<std::string>& symbols)
+void generate_wrapper(std::ofstream& output, const char* soname, const std::set<std::string>& symbols, const std::vector<std::string>& dependencies)
 {
 	output << "#include <elfcalls.h>\n"
 		"extern struct elf_calls* _elfcalls;\n\n"
 		"extern const char __elfname[];\n\n";
 
+    for (int i = 0; i < dependencies.size(); i++)
+    {
+	    output << "extern const char __elfname_" << i << "[];\n\n"
+	        "static void* lib_" << i << "_handle;\n"
+		    "static void initializer_" << i << "() {\n"
+		    "\tlib_" << i << "_handle = _elfcalls->dlopen_fatal(__elfname_" << i << ");\n"
+		    "}\n\n";
+    }
+
 	output << "static void* lib_handle;\n"
-		"__attribute__((constructor)) static void initializer() {\n"
-		"\tlib_handle = _elfcalls->dlopen_fatal(__elfname);\n"
+		"__attribute__((constructor)) static void initializer() {\n";
+	
+	for (int i = 0; i < dependencies.size(); i++)
+    {
+	    output << "\tinitializer_" << i << "();\n";
+    }
+	
+	output << "\tlib_handle = _elfcalls->dlopen_fatal(__elfname);\n"
 		"}\n\n";
+	
+	for (int i = 0; i < dependencies.size(); i++)
+    {
+	    output << "static void* lib_" << i << "_handle;\n"
+		    "static void destructor_" << i << "() {\n"
+		    "\t_elfcalls->dlclose_fatal(lib_" << i << "_handle);\n"
+		    "}\n\n";
+    }
 
 	output << "__attribute__((destructor)) static void destructor() {\n"
-		"\t_elfcalls->dlclose_fatal(lib_handle);\n"
-		"}\n\n";
+		"\t_elfcalls->dlclose_fatal(lib_handle);\n";
+	
+	for (int i = 0; i < dependencies.size(); i++)
+    {
+	    output << "\tdestructor_" << i << "();\n";
+    }
+	
+	output << "}\n\n";
 	
 	for (const std::string& sym : symbols)
 	{
@@ -258,8 +302,16 @@ void generate_wrapper(std::ofstream& output, const char* soname, const std::set<
 			"\treturn _elfcalls->dlsym_fatal(lib_handle, \"" << sym << "\");\n"
 			"}\n\n";
 	}
+
 	output << "asm(\".section __TEXT,__elfname\\n"
 		".private_extern ___elfname\\n"
 		"___elfname: .asciz \\\"" << soname << "\\\"\");\n";
+	
+	for (int i = 0; i < dependencies.size(); i++)
+    {
+	    output << "asm(\".section __TEXT,__elfname_" << i << "\\n"
+		    ".private_extern ___elfname_" << i << "\\n"
+		    "___elfname_" << i << ": .asciz \\\"" << dependencies[i] << "\\\"\");\n";
+    }
 }
 
